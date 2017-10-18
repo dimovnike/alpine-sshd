c84cd209d7808e83837d994ca708c7dceecc1839
nick dimov <3619341+dimovnike@users.noreply.github.com>
2017-10-18 22:29:04 +0300

    master: enabled sshd to run even if iptables not present

--- run-sshd.sh
+++ run-sshd.sh
@@ -17,9 +17,11 @@ if [ ! -d "/var/run/sshd" ]; then
   mkdir -p /var/run/sshd
 fi
 
-# allow ssh only from inside
-iptables -A INPUT  -i eth0 -p tcp --dport 22 -j ACCEPT
-iptables -A OUTPUT -o eth0 -p tcp --sport 22 -m state --state ESTABLISHED,RELATED -j ACCEPT
+if which iptables > /dev/null; then
+	# in case we have iptables, allow ssh only from inside (in case its used with openvpn layer)
+	iptables -A INPUT  -i eth0 -p tcp --dport 22 -j ACCEPT
+	iptables -A OUTPUT -o eth0 -p tcp --sport 22 -m state --state ESTABLISHED,RELATED -j ACCEPT
+fi
 
 /usr/sbin/sshd -D -e
 
